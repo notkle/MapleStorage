bf0f314182e0e6dc40cec6f71732763e6fcbb9f7
notkle <sotempyhehe@gmail.com>
2026-04-25 16:08:17 -0400

    Update app.js

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const CATEGORY_ORDER = [
   'Other',
 ];
 
-const API_BASE = '/api/maplestory/GMS/267';
+const API_BASE = '/api/maplestory/GMS/266';
 let storage = [];
 let pendingItem = null;
 let selectedClass = null;
